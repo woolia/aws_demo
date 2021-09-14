ca6e9e50c1458f69be0852861c24b8086ab044d9
woolia <zecrar@naver.com>
2021-09-15 01:25:24 +0900

    springboot-pr

--- src/main/java/project/demo/config/WebConfig.java
+++ src/main/java/project/demo/config/WebConfig.java
@@ -14,7 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
-
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginUserArgumentResolver);
